c3a428dbcd1858e2efc00e2b702894410eea686f
matiascabral <matiascabralmendez@gmail.com>
2022-08-16 11:49:34 -0300

    primer commit

---
+++ profile.js
@@ -0,0 +1,123 @@
+export const skills = [
+  {
+    skill: "Javascript",
+    percentage: 100,
+  },
+  {
+    skill: "React",
+    percentage: 80,
+  },
+  {
+    skill: "Node JS",
+    percentage: 90,
+  },
+  {
+    skill: "Next JS",
+    percentage: 60,
+  },
+  {
+    skill: "FireBase",
+    percentage: 60,
+  },
+  {
+    skill: "Css",
+    percentage: 90,
+  },
+  {
+    skill: "Saas",
+    percentage: 90,
+  },
+  {
+    skill: "Material UI",
+    percentage: 90,
+  },
+  {
+    skill: "Booststrap",
+    percentage: 100,
+  },
+  {
+    skill: "Chakra UI",
+    percentage: 80,
+  },
+];
+
+export const studies = [
+  {
+    title: "Desarrollo Web",
+    description:
+      "Sumergido en las mejores prácticas del desarrollo web, trabajando con HTML y CSS. Optimizando al máximo el desarrollo de sitios web, implementando prácticas de versionado de código con GIT.",
+    from: 2021,
+    to: 2022,
+  },
+  {
+    title: "JavaScript",
+    description: "Desarrollo Front End con JavaScript.",
+    from: 2022,
+    to: 2022,
+  },
+];
+
+export const experiences = [
+  {
+    title: "UI Design",
+    description:
+      "Responsable del diseño UI en proyecto multiplataforma ecommerce, desarrollado con Nextjs, Chakra UI",
+    from: 2022,
+    to: 2022,
+  },
+];
+
+export const projects = [
+  {
+    name: "Play Pókemon!",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+    link: "https://matiasc13.github.io/playPokemon-matiasCabral/",
+  },
+  {
+    name: "Discos Shop JS Vanilla",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio2.png",
+    link: "https://matiasc13.github.io/proyectoFinalJavaScript-matiasCabral/",
+  },
+  {
+    name: "Discos Shop React",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio3.png",
+    link: "http://discosmatishop.tk/",
+  },
+  {
+    name: "Ecommerce Next js",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio4.png",
+    link: "",
+  },
+];
+
+export const posts = [
+  {
+    title: "React",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Angular",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Nextjs",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+];
