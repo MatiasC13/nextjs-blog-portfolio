d91e3b42b93d705b3a6dd685c299af9ace0ae664
matiascabral <matiascabralmendez@gmail.com>
2022-08-19 18:16:53 -0300

    next config gmail pass

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    GMAIL_PASS: "oemnawgxnthbaszc",
+    GMAIL_PASS: "szpkpdbduuvwevex",
     GMAIL_USER: "servicio.notificaciones.compras@gmail.com",
     NEXT_PUBLIC_RECAPTCHA_SITE_KEY: "6LctJ4EhAAAAAMYiYrhx7ql6mo3KCMOx1F2mMHyG",
     RECAPTCHA_SECRET_KEY: "6LctJ4EhAAAAANZQ4CXfVWH6ZGqXxY6SagUJRBHI",
